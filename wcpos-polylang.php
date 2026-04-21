a8bba962b433c119e13872b2ab34a3c10235d257
Paul Kilmurray <paul@kilbot.com>
2026-04-21 16:02:59 +0200

    chore: add Update URI header and bump version (#15)
    * chore: add Update URI header
    
    * chore: bump version to 0.1.1

--- wcpos-polylang.php
+++ wcpos-polylang.php
@@ -2,8 +2,9 @@
 /**
  * Plugin Name: WCPOS Polylang Integration
  * Description: Polylang language filtering for WCPOS, including fast-sync route coverage and per-store language support in WCPOS Pro.
- * Version: 0.1.0
+ * Version: 0.1.1
  * Author: kilbot
+ * Update URI:  https://github.com/wcpos/wcpos-polylang
  * Requires Plugins: woocommerce, polylang
  * Text Domain: wcpos-polylang
  */
@@ -14,7 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-const VERSION = '0.1.0';
+const VERSION = '0.1.1';
 
 require_once __DIR__ . '/includes/class-plugin.php';
 
